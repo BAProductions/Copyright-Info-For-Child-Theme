5b9620502b6cf79f45faa536902fbd7e5487dc33
BAproductions <BAproductions@MacBook-Pro.local>
2017-09-14 13:13:35 -0400

    Copyright Info For Child Themes
    Copyright Info For Child Themes

--- copyright-info-for-child-themes.php
+++ copyright-info-for-child-themes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Copyright Info For Child Theme
 Plugin URI:  https://github.com/BAProductions/Copyright-Info-For-Child-Themes
-Description: This plugin allow you add copyright info to your child theme insted of you using the parent theme copy info section. just go apprence and an click crfc Theme the add the code aboved add to your footer.php save it then add text box reloade your site and done.
+Description: this plugin is used to add copyright info to a child theme whaen the maain has pore copyright info system to begin width. This is not a subtute for your themes copyright info system but if your has the main themes has a pore copyright info system then this is the way to go not ideal but is the onely way I found you can do it with adding files to the child theme and causing an error.
 Version:     0.1
 Author:      PressThemes/BAProductions/DJANHipHop
 Author URI:  https://github.com/BAProductions
